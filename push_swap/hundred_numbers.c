2edad62044a831585b97f9c82bfdd0b5e53bb311
Antonio Martin espinosa <antmarti@c1r15s3.42madrid.com>
2021-04-02 18:01:50 +0200

    in proccess

--- push_swap/hundred_numbers.c
+++ push_swap/hundred_numbers.c
@@ -6,7 +6,7 @@
 /*   By: antmarti <antmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 17:59:13 by antmarti          #+#    #+#             */
-/*   Updated: 2021/04/01 19:30:33 by antmarti         ###   ########.fr       */
+/*   Updated: 2021/04/02 17:55:17 by antmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ int	check_order(int *arr, int len)
 	int	i;
 
 	i = 0;
-	while (i + 1 < len)
+	while (len - 1 > i)
 	{
 		if (arr[i] > arr[i + 1])
 			return (1);
 		i++;
+	//printf("******%d-----%d\n", arr[i], arr[i + 1]);
 	}
 	return (0);
 }
@@ -41,95 +42,37 @@ int	get_mid_point(int *arr, int len)
 	return (sum / len);
 }
 
-int	move_number_rev(t_swap *swap, int *bool)
+void	chunk_pb(t_swap *swap, int num)
 {
-	int	min;
 	int	i;
-	int	pos;
 
-	min = 0;
-	i = 0;
-	pos = 0;
-	while (i < swap->b_elem)
+	i = swap->chunks_len[num];
+	if (swap->a_elem >= 1)
 	{
-		if (swap->nums[0][0] > swap->nums[1][i])
+		while (i > 0)
 		{
-			if (!*bool)
-			{
-				min = swap->nums[1][i];
-				pos = i;
-				*bool = 1;
-			}
-			else if (min < swap->nums[1][i])
-			{
-				min = swap->nums[1][i];
-				pos = i;
-			}
+			swap->nums[num + 2][i] = swap->nums[num + 2][i - 1];
+			i--;
 		}
-		i++;
+		swap->nums[num + 2][0] = swap->nums[0][0];
+		swap->chunks_len[num]++;
 	}
-	return (pos);
 }
 
-void	order_b(t_swap *swap)
+void	chunk_pa(t_swap *swap, int num)
 {
-	int	pos;
-	int	bool;
+	int	i;
 
-	bool = 0;
-	pos = move_number_rev(swap, &bool);
-	if (bool)
-	{
-		if (pos < swap->b_elem / 2)
-		{
-			while (pos > 0)
-			{
-				r_opt(swap, 7);
-				pos--;
-			}
-		}
-		else
-		{
-			while (pos < swap->b_elem)
-			{
-				rr_opt(swap, 10);
-				pos++;
-			}
-		}
-	}
-	else
-	{
-		pos = 1;
-		while (swap->nums[1][pos - 1] > swap->nums[1][pos]
-			&& pos < swap->b_elem)
-			pos++;
-		if (pos != swap->b_elem)
-		{
-			if (pos < swap->b_elem / 2)
-			{
-				while (pos > 0)
-				{
-					r_opt(swap, 7);
-					pos--;
-				}
-			}
-			else
-			{
-				while (pos < swap->b_elem)
-				{
-					rr_opt(swap, 10);
-					pos++;
-				}
-			}
-		}
-	}
-	pb_opt(swap);
-}
-
-void	get_number_top(t_swap *swap, int i)
-{
 	i = 0;
-	order_b(swap);
+	if (swap->chunks_len[num] >= 1)
+	{
+		while (i < swap->chunks_len[num])
+		{
+			swap->nums[num + 2][i] = swap->nums[num + 2][i + 1];
+			i++;
+		}
+		swap->chunks_len[num]--;
+	}
 }
 
 void	hundred_numbers(t_swap *swap)
@@ -137,11 +80,15 @@ void	hundred_numbers(t_swap *swap)
 	int	i;
 	int	j;
 	int	mdp;
+	int	len_a;
 
 	sort(swap);
 	i = -1;
 	while (++i < swap->a_elem)
+	{
+		//printf("POS: %d    NUM %d\n", swap->pos[i], swap->nums[0][i]);
 		swap->nums[0][i] = swap->pos[i];
+	}
 	i = -1;
 	j = -1;
 	mdp = 0;
@@ -151,27 +98,37 @@ void	hundred_numbers(t_swap *swap)
 		{
 			j = -1;
 			i++;
-			swap->chunks_len[i] = swap->a_elem / 2;
+			//printf("********%d\n", i);
+			swap->chunks_len[i] = 0;
 			mdp = get_mid_point(swap->nums[0], swap->a_elem);
-			while (++j < swap->a_elem / 2)
+			len_a = swap->a_elem / 2;
+			while (swap->chunks_len[i] < len_a - 1)
 			{
+				//printf("-----%d    %d\n", swap->chunks_len[i], len_a);
 				if (swap->nums[0][0] < mdp)
+				{
+					chunk_pb(swap, i);
 					pb_opt(swap);
+				}
 				else if (swap->nums[0][swap->a_elem - 1]
 					< mdp)
 				{
 					rr_opt(swap, 9);
+					chunk_pb(swap, i);
 					pb_opt(swap);
 				}
 				else
 					r_opt(swap, 6);
 			}
 		}
+		else
+			break ;
 		if (swap->a_elem < 3)
 		{
 			if (check_order(swap->nums[0], swap->a_elem))
 				s_opt(swap, 1);
 			break ;
 		}
+		//printf("a elem: %d\n", swap->a_elem);
 	}
 }
